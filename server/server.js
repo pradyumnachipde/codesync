f0c8a8f2bba44e680040af9091d65a375eae7af7
Pradyumna Chipde <pradyumnachipde41@gmail.com>
2026-06-26 10:22:58 +0530

    Add JWT Middleware and protected profile route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
